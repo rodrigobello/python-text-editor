eb81eac2cacf6f29ad5b7dd95a7985040d616f5d
Rodrigo Bello <rodrigoabbello@gmail.com>
2020-08-25 14:51:01 -0300

    Add text navigation through the Cursor object

--- editor.py
+++ editor.py
@@ -8,34 +8,75 @@ class Editor:
             line.replace("\n", "")
             for line in open("random.txt").readlines()
         ]
-        print(lines)
+        self.buffer = Buffer(lines)
+        self.cursor = Cursor()
 
     def run(self):
-        while True:
-            self.render()
-            self.handle_input()
-
-    def render(self):
-        pass
-
-    def handle_input(self):
         fd = sys.stdin.fileno()
         old_settings = termios.tcgetattr(fd)
         try:
             tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(1)
+            while True:
+                self.render()
+                self.handle_input()
         finally:
             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        if ch == chr(17):
+
+    def render(self):
+        ANSI.clear_screen()
+        ANSI.move_cursor(0, 0)
+        self.buffer.render()
+        ANSI.move_cursor(self.cursor.row, self.cursor.col)
+
+    def handle_input(self):
+        ch = sys.stdin.read(1)
+        if ch == chr(17): # ctrl+q
            sys.exit(0)
+        if ch == 'k':
+            self.cursor = self.cursor.up()
+        if ch == 'j':
+            self.cursor = self.cursor.down()
+        if ch == 'h':
+            self.cursor = self.cursor.left()
+        if ch == 'l':
+            self.cursor = self.cursor.right()
+
+
+class ANSI:
+    @classmethod
+    def clear_screen(cls):
+        print("[2J")
+
+    @classmethod
+    def move_cursor(cls, row, col):
+        print(f"[{row + 1};{col + 1}H")
 
 
 class Buffer:
-    pass
+    def __init__(self, lines):
+        self.lines = lines
+
+    def render(self):
+        for line in self.lines:
+            print(line, end='\r\n')
 
 
 class Cursor:
-    pass
+    def __init__(self, row=0, col=0):
+        self.row = row
+        self.col = col
+
+    def up(self):
+        return Cursor(self.row - 1, self.col)
+
+    def down(self):
+        return Cursor(self.row + 1, self.col)
+
+    def left(self):
+        return Cursor(self.row, self.col - 1)
+
+    def right(self):
+        return Cursor(self.row, self.col + 1)
 
 
 if __name__ == '__main__':
