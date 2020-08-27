2dc52074052743a4ca96b71b59574150d0125011
Rodrigo Bello <rodrigoabbello@gmail.com>
2020-08-27 00:37:08 -0300

    Add editor history allowing to undo changes

--- editor.py
+++ editor.py
@@ -10,6 +10,7 @@ class Editor:
         ]
         self.buffer = Buffer(lines)
         self.cursor = Cursor()
+        self.history = []
 
     def run(self):
         fd = sys.stdin.fileno()
@@ -32,9 +33,12 @@ class Editor:
         ANSI.move_cursor(self.cursor.row, self.cursor.col)
 
     def handle_input(self):
+        # ASCII TABLE: http://www.physics.udel.edu/~watson/scen103/ascii.html
         ch = sys.stdin.read(1)
         if ch == chr(17): # ctrl+q
            sys.exit(0)
+        elif ch == chr(21): # ctrl+u
+            self.restore_snapshot()
         elif ch == chr(16): # ctrl+p
             self.cursor = self.cursor.up(self.buffer)
         elif ch == chr(14): # ctrl+n
@@ -44,15 +48,25 @@ class Editor:
         elif ch == chr(6): # ctrl+f
             self.cursor = self.cursor.right(self.buffer)
         elif ch == chr(13): # enter
+            self.save_snapshot()
             self.buffer = self.buffer.split_line(self.cursor.row, self.cursor.col)
             self.cursor = self.cursor.down(self.buffer).move_to_col(0)
         elif ch == chr(127): # backspace
             if self.cursor.col > 0:
+                self.save_snapshot()
                 self.buffer = self.buffer.delete(self.cursor.row, self.cursor.col - 1)
                 self.cursor = self.cursor.left(self.buffer)
         else:
+            self.save_snapshot()
             self.buffer = self.buffer.insert(ch, self.cursor.row, self.cursor.col)
             self.cursor = self.cursor.right(self.buffer)
+
+    def save_snapshot(self):
+        self.history.append([self.buffer, self.cursor])
+
+    def restore_snapshot(self):
+        if self.history:
+            self.buffer, self.cursor = self.history.pop()
 
 
 class ANSI:
